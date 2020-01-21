b20bfec3d0de467d84e472d3a95e5ef9558f1d05
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-21 14:50:44 -0600

    Added instructor class

---
+++ instructor.py
@@ -0,0 +1,21 @@
+"""
+You must define a type for representing an instructor in code.
+
+First name
+Last name
+Slack handle
+The instructor's cohort
+The instructor's specialty (e.g. dad jokes, excitement, dancing, etc.)
+A method to assign an exercise to a student
+"""
+
+class Instructor:
+    def __init__ (self, first_name,last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.slack_handle = ""
+        self.cohort = ""
+        self.specialty = ""
+
+    def assign_exercise (self, student, exercise):
+        pass
