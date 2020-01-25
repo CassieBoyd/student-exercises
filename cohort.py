bf12100b273fa10a410b4dc90885d784eaed5593
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-25 13:45:57 -0600

    Added notes to cohort and added missing colon

--- cohort.py
+++ cohort.py
@@ -6,8 +6,9 @@ The collection of students in the cohort.
 The collection of instructors in the cohort.
 """
 
+# students and instructors are set to empty lists because a cohort has many students and instructors
 class Cohort:
-    def __init__ (self, name)
+    def __init__ (self, name):
         self.name = name
         self.students = []
         self.instructors = []
