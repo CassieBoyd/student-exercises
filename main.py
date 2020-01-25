9a1d7771c40bbe68ac23342eaec905e67c4f67f8
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-25 15:25:42 -0600

    Created students and added them to cohorts

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ sand_the_floor = Exercise("Sand The Floor", "JavaScript")
 wax_on = Exercise("Wax On", "Python")
 wax_off = Exercise("Wax Off", "Python")
 
-day_cohort_38 = Cohort("Day Cohort 38")
+day_cohort_98 = Cohort("Day Cohort 98")
 cobra_kai = Cohort("Night Cohort Cobra Kai")
 miyagi_dojo = Cohort("Day Cohort Miyagi Dojo")
+
+daniel_larusso = Student("Daniel", "LaRusso", "Daniel-san", "Day Cohort Miyagi Dojo")
+miyagi_dojo.students.append(daniel_larusso)
+johhny_lawrence = Student("Johnny", "Lawrence", "Kn335w33p3r", "Night Cohort Cobra Kai")
+cobra_kai.students.append(johhny_lawrence)
+john_doe = Student("John", "Doe", "DoeBoi", "Day Cohort 98")
+day_cohort_98.students.append(john_doe)
+janet_doe = Student("Janet", "Doe", "DammitJanet", "Day Cohort 98")
+day_cohort_98.students.append(janet_doe)
+
+print(cobra_kai.students[0].first_name)
